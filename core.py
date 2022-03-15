dac1dda02ca09ee87e0f17ffee02a2b20eaec8bf
Denis_Odnorozhenko <denis_odnorozhenko@epam.com>
2022-03-15 19:30:08 +0300

    added BDoc sources

--- core.py
+++ core.py
@@ -13,7 +13,6 @@ import cv2
 from PIL import Image
 import streamlit as st
 
-
 # fuck this line :)
 # reason 
 # File "/home/research/table-transformer/detr/engine.py", line 12, in <module>
@@ -43,14 +42,27 @@ from table_datasets import (
 def get_class_map():
     class_map = {
         "table": 0,
-         "table column": 1, #red
-        "table row": 2, #blue
-        "table column header": 3, #magenta
-        "table projected row header": 4, #cyan
+        "table column": 1,  # red
+        "table row": 2,  # blue
+        "table column header": 3,  # magenta
+        "table projected row header": 4,  # cyan
         "table spanning cell": 5,
         "no object": 6,
     }
     return class_map
+
+
+def get_colors_map():
+    colors_map = [
+        {'name': 'brown', 'category_name': 'table', 'r': 128, 'g': 64, 'b': 64, 'dx': 3, 'dy': 3},
+        {'name': 'red', 'category_name': 'table column', 'r': 255, 'g': 0, 'b': 0, 'dx': 4, 'dy': 4},
+        {'name': 'blue', 'category_name': 'table row', 'r': 0, 'g': 0, 'b': 255, 'dx': 3, 'dy': 3},
+        {'name': 'magenta', 'category_name': 'table column header', 'r': 255, 'g': 0, 'b': 255, 'dx': 1, 'dy': 1},
+        {'name': 'cyan', 'category_name': 'table projected row header', 'r': 0, 'g': 255, 'b': 255, 'dx': 2, 'dy': 2},
+        {'name': 'green', 'category_name': 'table spanning cell', 'r': 0, 'g': 255, 'b': 0, 'dx': 3, 'dy': 3},
+        {'name': 'orange', 'category_name': 'other', 'r': 255, 'g': 127, 'b': 39, 'dx': 3, 'dy': 3}
+    ]
+    return colors_map
 
 
 def get_model(args, device):
@@ -73,7 +85,6 @@ def get_model(args, device):
         model_state_dict.update(pretrained_dict)
         model.load_state_dict(model_state_dict, strict=True)
     return model, criterion, postprocessors
-
 
 
 # def main():
@@ -102,40 +113,34 @@ class TableRecognizer:
 
         self.normalize = R.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
-    def rescale_bboxes(self,out_bbox, size):
+    def rescale_bboxes(self, out_bbox, size):
         img_w, img_h = size
         b = self.box_cxcywh_to_xyxy(out_bbox)
         b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
         return b
 
-    def box_cxcywh_to_xyxy(self,x):
+    def box_cxcywh_to_xyxy(self, x):
         x_c, y_c, w, h = x.unbind(1)
         b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
         return torch.stack(b, dim=1)
 
-    def predict(self, image_path = None, page_tokens=None, debug=True, thresh=0.9):
-        if image_path is None:
-            image_path = "/data/pubtables1m/PubTables1M-Structure-PASCAL-VOC/images/PMC514496_table_0.jpg"  
+    def origin_img_cell_xy(self, pred_cells, crop_box, padding_box):
+        for cell in pred_cells:
+            cell['bbox'] = list(np.array(cell['bbox']) + np.array(cropped_bbox) - np.array(padding))
+            for span in cell['spans']:
+                span['bbox'] = list(np.array(span['bbox']) + np.array(cropped_bbox) - np.array(padding))
+        return pred_cells
 
-        image = image_path
-        if isinstance(image_path, str):
-            image = Image.open(image_path).convert("RGB")
+    def origin_img_table_xy(self, items, crop_box, padding_box):
+        for item in items:
+            item['bbox'] = list(np.array(item['bbox']) + np.array(cropped_bbox) - np.array(padding))
+        return items
 
-        w, h = image.size
-
-        img_tensor = self.normalize(F.to_tensor(image))[0]
-        img_tensor = torch.unsqueeze(img_tensor, 0).to(self.device)
-
-        outputs = None
-        with torch.no_grad():
-            outputs = self.model(img_tensor)
-
-        ############# from grits ##################
-
-        """
-          This function runs the GriTS proposed in the paper. We also have a debug
-          mode which let's you see the outputs of a model on the pdf pages.
-          """
+    def objects_to_grid_cells(self, outputs):
+        """     from grits
+                 This function runs the GriTS proposed in the paper. We also have a debug
+                 mode which let's you see the outputs of a model on the pdf pages.
+                 """
         structure_class_names = [
             'table', 'table column', 'table row', 'table column header',
             'table projected row header', 'table spanning cell', 'no object'
@@ -154,16 +159,37 @@ class TableRecognizer:
         m = outputs['pred_logits'].softmax(-1).max(-1)
         scores = m.values
         labels = m.indices
-        #rescaled_bboxes = rescale_bboxes(torch.tensor(boxes[0], dtype=torch.float32), img_test.size)
-        rescaled_bboxes = self.rescale_bboxes(boxes[0].cpu(), image.size)
+        # rescaled_bboxes = rescale_bboxes(torch.tensor(boxes[0], dtype=torch.float32), img_test.size)
+        rescaled_bboxes = self.rescale_bboxes(boxes[0].cpu(), image.size)  # TODO validate
         pred_bboxes = [bbox.tolist() for bbox in rescaled_bboxes]
         pred_labels = labels[0].tolist()
         pred_scores = scores[0].tolist()
-        pred_table_structures, pred_cells, pred_confidence_score = objects_to_cells(pred_bboxes, pred_labels, pred_scores,
+        pred_table_structures, pred_cells, pred_confidence_score = objects_to_cells(pred_bboxes, pred_labels,
+                                                                                    pred_scores,
                                                                                     page_tokens, structure_class_names,
-                                                                                    structure_class_thresholds, structure_class_map)
+                                                                                    structure_class_thresholds,
+                                                                                    structure_class_map)
 
-        ############# from grits ##################
+        return pred_table_structures, pred_cells, pred_confidence_score
+
+    def predict(self, image_path=None, page_tokens=None, debug=True, thresh=0.9):
+        if image_path is None:
+            image_path = "/data/pubtables1m/PubTables1M-Structure-PASCAL-VOC/images/PMC514496_table_0.jpg"
+
+        image = image_path
+        if isinstance(image_path, str):
+            image = Image.open(image_path).convert("RGB")
+
+        w, h = image.size
+
+        img_tensor = self.normalize(F.to_tensor(image))[0]
+        img_tensor = torch.unsqueeze(img_tensor, 0).to(self.device)
+
+        outputs = None
+        with torch.no_grad():
+            outputs = self.model(img_tensor)
+
+        pred_table_structures, pred_cells, pred_confidence_score = self.objects_to_grid_cells(outputs)
 
         image_size = torch.unsqueeze(torch.as_tensor([int(h), int(w)]), 0).to(self.device)
         results = self.postprocessors['bbox'](outputs, image_size)[0]
@@ -177,20 +203,12 @@ class TableRecognizer:
 
                 xmin, ymin, xmax, ymax = list(map(int, results["boxes"][idx]))
                 category_type = results["labels"][idx].item()
-                colors = [
-                    {'name': 'brown','r':128,'g':64,'b':64,'dx':3,'dy':3},
-                    {'name': 'red', 'r': 255, 'g': 0, 'b': 0, 'dx': 4, 'dy': 4},
-                    {'name': 'blue', 'r': 0, 'g': 0, 'b': 255, 'dx': 3, 'dy': 3},
-                    {'name': 'magenta', 'r': 255, 'g': 0, 'b': 255, 'dx': 1, 'dy': 1},
-                    {'name': 'cyan', 'r': 0, 'g': 255, 'b': 255, 'dx': 2, 'dy': 2},
-                    {'name': 'green', 'r': 0, 'g': 255, 'b': 0, 'dx': 3, 'dy': 3},
-                    {'name': 'orange', 'r': 255, 'g': 127, 'b': 39, 'dx': 3, 'dy': 3}
-                   ]
+                colors = get_colors_map()
 
-                r,g,b = colors[category_type]['r'], colors[category_type]['g'], colors[category_type]['b']
-                dx,dy = colors[category_type]['dx'], colors[category_type]['dy']
+                r, g, b = colors[category_type]['r'], colors[category_type]['g'], colors[category_type]['b']
+                dx, dy = colors[category_type]['dx'], colors[category_type]['dy']
 
-                cv2.rectangle(image, (xmin, ymin), (xmax+dx, ymax+dy), (r,g,b), 2)
+                cv2.rectangle(image, (xmin, ymin), (xmax + dx, ymax + dy), (r, g, b), 2)
                 result_objects.append({'category_type': category_type, 'bbox': [xmin, ymin, xmax, ymax]})
             results["debug_image"] = image
             results["debug_objects"] = result_objects
@@ -204,10 +222,10 @@ def main():
     import glob
     from tqdm import tqdm
 
-    for image_path in tqdm(glob.glob("/data/pubtables1m/PubTables1M-Structure-PASCAL-VOC/images/*.jpg")[:100], total=100): 
+    for image_path in tqdm(glob.glob("/data/pubtables1m/PubTables1M-Structure-PASCAL-VOC/images/*.jpg")[:100],
+                           total=100):
         output = m.predict(image_path)
         cv2.imwrite(f"debug/{os.path.basename(image_path)}.jpg", output["debug_image"])
-
 
 
 if __name__ == "__main__":
--- src/text_flow.py
+++ src/text_flow.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 import numpy as np
 import cv2
+import json
 
 from copy import deepcopy
 from typing import Any, List, Optional, Tuple
@@ -71,168 +72,8 @@ def stitch_text_tokens_into_lines(
     return convert_to_bd(merged_boxes)  # type: ignore
 
 
-def find_gap(
-    lines: List[Token],
-) -> List[Token]:
-    """
-    Detect gaps between two columns of text
-    """
-    # sort lines by Y
-    lines.sort(key=lambda x: x.bbox[1])
-
-    gaps: List[Token] = []
-    while len(lines) > 10:
-        merged_intervals = merge_bbox_intervals(deepcopy(lines))
-        gaps = define_gaps_between_intervals(merged_intervals, lines, gaps)
-        lines.pop(0)
-    return gaps
 
 
-def merge_bbox_intervals(intervals: List[Token]) -> List[Token]:
-    intervals.sort(key=lambda x: x.bbox[0])
-    i = 0
-    j = 1
-    while j < len(intervals):
-        if intervals[i].bbox[2] > intervals[j].bbox[2]:
-            intervals.pop(j)
-        elif intervals[i].bbox[2] < intervals[j].bbox[0]:
-            i += 1
-            j += 1
-        else:
-            tmp = intervals.pop(j)
-            intervals[i].bbox = [
-                intervals[i].bbox[0],
-                intervals[i].bbox[1],
-                tmp.bbox[2],
-                intervals[i].bbox[3],
-            ]
-    return intervals
-
-
-def insert_gap(gap: Token, gaps: List[Token]) -> List[Token]:
-    i = 0
-    while i < len(gaps):
-        if bbox_inside_another(gap, gaps[i]):
-            return gaps
-        elif bbox_inside_another(gaps[i], gap):
-            gaps.pop(i)
-        i += 1
-    gaps.append(gap)
-    return gaps
-
-
-def define_gaps_between_intervals(
-    intervals: List[Token],
-    lines_for_x: List[Token],
-    gaps: List[Token],
-) -> List[Token]:
-    l_old = intervals.pop(0)
-    while intervals:
-        l = intervals.pop(0)
-        gap = Token(
-            type="gap",
-            bbox=[
-                l_old.bbox[2],
-                lines_for_x[0].bbox[1],
-                l.bbox[0],
-                lines_for_x[-1].bbox[3],
-            ],
-            text="",
-        )
-        insert_gap(gap, gaps)
-        l_old = l
-    return gaps
-
-
-def find_paragraphs(
-    gaps: List[Token], page_height: float, page_width: float
-) -> List[Token]:
-    # Add the general paragraphs. It will contain all other paragraphs.
-    paragraphs = [
-        Token(
-            bbox=[0, 0, page_width, page_height],
-            type="paragraph",
-            children=[],
-            text="",
-        )
-    ]
-
-    if not gaps:
-        return paragraphs
-
-    # Sort gaps. Paragraphs (and gaps between paragraphs) can be nested.
-    # The shortest gaps always is the deepest one.
-    gaps.sort(key=lambda x: x.bbox[3] - x.bbox[1])
-
-    # Add left and right borders. They are longer then any real gaps
-    gaps.append(Token(bbox=[0, 0, 0, page_height + 1], type="gap", text=""))
-    gaps.append(
-        Token(
-            bbox=[page_width, 0, page_width, page_height + 1],
-            type="gap",
-            text="",
-        )
-    )
-
-    while len(gaps) > 2:
-        # Take the shortest gap
-        gap = gaps.pop(0)
-
-        # Find paragraphs to the left and to the right about gap.
-        # Left (for left paragraph) and right(for right) border will define later.
-        paragraph_left = Token(
-            bbox=[0, gap.bbox[1], gap.bbox[0], gap.bbox[3]],
-            type="paragraph",
-            children=[],
-            text="",
-        )
-        paragraph_right = Token(
-            bbox=[gap.bbox[2], gap.bbox[1], page_width, gap.bbox[3]],
-            type="paragraph",
-            children=[],
-            text="",
-        )
-
-        # find borders
-        for g in gaps:
-            paragraph_left.bbox[0] = (
-                max(paragraph_left.bbox[0], g.bbox[2])
-                if max(paragraph_left.bbox[0], g.bbox[2])
-                < paragraph_left.bbox[2]
-                else paragraph_left.bbox[0]
-            )
-            paragraph_right.bbox[2] = (
-                min(paragraph_right.bbox[2], g.bbox[0])
-                if min(paragraph_right.bbox[2], g.bbox[0])
-                > paragraph_right.bbox[0]
-                else paragraph_right.bbox[2]
-            )
-
-        paragraphs.append(paragraph_left)
-        paragraphs.append(paragraph_right)
-
-    return paragraphs
-
-
-def get_nested_paragraphs(
-    tokens: List[Token], paragraphs: List[Token]
-) -> List[Token]:
-    # Fill the smallest paragraph first.
-    paragraphs.sort(key=lambda x: x.bbox[3] - x.bbox[1])
-
-    for parag in paragraphs[:]:
-        for token in tokens[:]:
-            if bbox_inside_another(token, parag):
-                tokens.remove(token)
-                parag.children.append(token)
-
-        paragraphs.remove(parag)
-        if not parag.children:
-            logger.error("Paragraph has no children")
-
-        tokens.append(parag)
-
-    return tokens
 
 
 def stitch_bd_bboxes_into_lines_as_boxes(
@@ -245,43 +86,8 @@ def stitch_bd_bboxes_into_lines_as_boxes(
     return convert_to_bd(merged_boxes)  # type: ignore
 
 
-def bbox_inside_another(box1: Token, box2: Token) -> bool:
-    """Box1 inside box2"""
-    bbox1 = BorderBox(
-        top_left_x=round(box1.bbox[0]),
-        top_left_y=round(box1.bbox[1]),
-        bottom_right_x=round(box1.bbox[2]),
-        bottom_right_y=round(box1.bbox[3]),
-    )
-    bbox2 = BorderBox(
-        top_left_x=round(box2.bbox[0]),
-        top_left_y=round(box2.bbox[1]),
-        bottom_right_x=round(box2.bbox[2]),
-        bottom_right_y=round(box2.bbox[3]),
-    )
-    return bbox1.box_is_inside_box(bbox2, threshold=0.5)  # type: ignore
 
-
-def get_ordered_text(objects: List[Token]) -> List[Token]:
-    result = []
-
-    def sort_text(tokens: List[Token]) -> None:
-        tokens = stitch_bd_bboxes_into_lines_as_boxes(
-            tokens, min_y_overlap_ratio=0.6
-        )
-
-        for token in tokens:
-            if not token.children:
-                result.append(token)
-            else:
-                sort_text(token.children)
-
-    sort_text(objects)
-    return result
-
-#TODO: move to my code
-import json
-def draw_bboxes2(
+def save_to_file(
     tokens: List[Token],
     image_path=None,
     path_to_save=None,
@@ -343,27 +149,7 @@ def arrange_text(
 
     lines = stitch_text_tokens_into_lines(tokens)
    # if verbose:
-    (w,h) = draw_bboxes2(lines, image_path,save_path, "lines")
+    (w,h) = save_to_file(lines, image_path, save_path, "lines")
 #        save_to_file(lines, "lines")
-
-    #gaps = find_gap(lines)
-    #if verbose:
-    #    draw_bboxes2(gaps, image_path, save_path,"gaps")
-    #    save_to_file(gaps, "gaps")
-
-    #paragraphs = find_paragraphs(gaps, h, w)
-    #if verbose:
-     #   draw_bboxes2(paragraphs, image_path,save_path, "paragraphs")
-     #   save_to_file(paragraphs,  "paragraphs")
-
-    #nested_paragraphs = get_nested_paragraphs(tokens, paragraphs)
-    #if verbose:
-    #    draw_bboxes2(nested_paragraphs, image_path,save_path, "nested_paragraphs")
-    #    save_to_file(nested_paragraphs, "nested_paragraphs")
-
-    #result = get_ordered_text(nested_paragraphs)
-    #if verbose:
-     #   draw_bboxes2(result, image_path,save_path, "result")
-     #   save_to_file(result,  "result")
 
     return import_to_text_tokens(lines)
