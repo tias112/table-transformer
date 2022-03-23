d9e06c4aa946825a78c307dafa01c17f70b36a18
Denis_Odnorozhenko <denis_odnorozhenko@epam.com>
2022-03-24 01:31:18 +0300

    add tables

--- detr/engine.py
+++ detr/engine.py
@@ -94,6 +94,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         outputs = model(samples)
+        print(outputs)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
 
