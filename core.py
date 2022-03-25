17c57d4ca2ac52fc0a6fcc906e71dab1b20df346
Denis_Odnorozhenko <denis_odnorozhenko@epam.com>
2022-03-25 17:00:38 +0300

    add tables

--- core.py
+++ core.py
@@ -228,6 +228,7 @@ class TableRecognizer:
             cols = self.origin_img_table_xy(cols, crop_box, padding_box)
             cells = self.origin_img_cell_xy(cells, crop_box, padding_box)
             headers =self.origin_img_cell_xy(headers, crop_box, padding_box)
+        print("headers", headers)
         return rows, cols, headers, cells, tables, output["debug_image"]
 
     def process_coco(self, max_count):
@@ -265,8 +266,8 @@ class TableRecognizer:
             labels = [self.cell_label(cell) for cell in cells]
             labels.extend([row['label'] for row in rows])
             labels.extend([col['label'] for col in cols])
-            labels.extend([table['label'] for table in tables])
             labels.extend([header['label'] for header in headers])
+            labels.extend([table['label'] for table in tables])
             for bbox, label in zip(bboxes, labels):
                 ann = {'area': (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]),
                        'iscrowd': 0,
